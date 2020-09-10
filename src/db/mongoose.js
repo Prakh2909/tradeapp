96aeadad5abf792fcaae156ba81241cbc358525b
Parker29 <prakharagarwal2909@gmail.com>
2020-09-10 20:26:16 +0530

    Fixed error in mongoose config

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-mongoose.connect('process.env.MONGODB_URL',{
+mongoose.connect(process.env.MONGODB_URL,{
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
